02e82dbe3cd0842a58f255a67fab74ff35b87403
keighleykodric <keighleykodric@gmail.com>
2021-11-29 20:25:13 -0800

    another one
    eyeroll

--- components/index.js
+++ components/index.js
@@ -5,6 +5,6 @@ export * as Care from './Care/Care';
 export * as Collapse from './Collapse/Collapse';
 export * as Copyright from './Copyright/Copyright';
 export * as Divider from './Divider/Divider';
-export * as Faq from './Faq/Faq';
+// export * as Faq from './Faq/Faq';
 export * as Header from './Header/Header';
 export * as Menu from './Menu/Menu';
